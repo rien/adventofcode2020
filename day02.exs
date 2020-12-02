2b97ef90e3e751db1ba91e307b46869164026f31
Rien Maertens <rien.maertens@posteo.be>
2020-12-02 14:25:07 +0100

    Day 02: part 2

--- day02.exs
+++ day02.exs
@@ -2,19 +2,36 @@ defmodule DayTwo do
   def solve(lines) do
     [
       first: solve_first(lines),
+      second: solve_second(lines),
     ]
   end
 
   def solve_first(lines) do
-    Enum.count(lines, &matches_policy(&1))
+    Enum.count(lines, &matches_first_policy(&1))
   end
 
-  def matches_policy(line) do
+  def solve_second(lines) do
+    Enum.count(lines, &matches_second_policy(&1))
+  end
+
+  def parse_policy(line) do
     [range, condition, password ] = String.split(line, " ")
     [start, stop] = String.split(range, "-") |> Enum.map(&String.to_integer(&1))
     char = String.at(condition, 0)
-    count = String.graphemes(password) |> Enum.count(fn x -> x == char end)
+    [start: start, stop: stop, char: char, password: String.graphemes(password)]
+  end
+
+  def matches_first_policy(line) do
+    [start: start, stop: stop, char: char, password: pw] = parse_policy(line)
+    count = Enum.count(pw, fn x -> x == char end)
     start <= count and count <= stop
+  end
+
+  def matches_second_policy(line) do
+    [start: start, stop: stop, char: char, password: pw] = parse_policy(line)
+    p1 = Enum.at(pw, start - 1) == char
+    p2 = Enum.at(pw, stop - 1) == char
+    (p1 and not p2) or (not p1 and p2)
   end
 end
 
